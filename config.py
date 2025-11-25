37d20ce4eed8645bc20f60c8c41b60fb365a641e
Piero Ramirez <ww.sco.lol@gmail.com>
2025-11-25 08:50:21 -0500

    Usar DATABASE_URL de Render para la conexión a Postgres

--- config.py
+++ config.py
@@ -2,15 +2,4 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key")
-
-
-def get_db_params():
-    return {
-        "host": os.environ["PGHOST"],
-        "dbname": os.environ["PGDATABASE"],  # 👈 OJO AQUÍ
-        "user": os.environ["PGUSER"],
-        "password": os.environ["PGPASSWORD"],
-        "port": os.environ.get("PGPORT", "5432"),
-        "sslmode": "require"
-    }
-
+    DATABASE_URL = os.environ.get("DATABASE_URL")
